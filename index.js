ce846cf9f6e2625d995a210136c7824fe5a540d4
Faysal Ahmed <info.faysal.32@gmail.com>
2025-05-21 20:51:16 +0600

    added delete method to delete user room info

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/roomInfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
